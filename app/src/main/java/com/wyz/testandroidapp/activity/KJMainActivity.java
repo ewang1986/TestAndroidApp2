bd9a9a616061a6b6d13c26084f819f334ed04fe2
wyz <2521801@qq.com>
2017-01-19 14:52:23 +0800

    简单使用kjframe

--- app/src/main/java/com/wyz/testandroidapp/activity/KJMainActivity.java
+++ app/src/main/java/com/wyz/testandroidapp/activity/KJMainActivity.java
@@ -1,17 +1,43 @@
 package com.wyz.testandroidapp.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.wyz.testandroidapp.R;
+
+import org.kymjs.kjframe.KJActivity;
+import org.kymjs.kjframe.KJBitmap;
+import org.kymjs.kjframe.ui.BindView;
 
 /**
  * Created by Administrator on 2017/1/19.
  */
-public class KJMainActivity extends Activity{
+public class KJMainActivity extends KJActivity{
+    @BindView(id = R.id.iv_kj)
+    public ImageView iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    private void init() {
+        String iv_url = "http://www.yunmai11.com/tpl/System/Static/images/logo-pigcms.png";
+        KJBitmap.Builder build = new KJBitmap.Builder();
+        build.imageUrl(iv_url);
+        build.view(iv);
+        build.display(new KJBitmap());
+    }
+
+    @Override
+    public void setRootView() {
         setContentView(R.layout.test_layout);
+    }
+
+    @Override
+    public void initWidget() {
+        super.initWidget();
+        init();
     }
 }
