f270527bb04aa1a1670ca318ffb4f45b6057361a
wyz <2521801@qq.com>
2017-01-22 17:32:11 +0800

    Fresco Uri 圆形图片

--- app/src/main/java/com/wyz/testandroidapp/activity/FrescoAssetsActivity.java
+++ app/src/main/java/com/wyz/testandroidapp/activity/FrescoAssetsActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.facebook.drawee.generic.RoundingParams;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.wyz.testandroidapp.R;
 
@@ -27,7 +28,9 @@ public class FrescoAssetsActivity extends Activity{
     }
 
     private void init() {
-        Uri uri = Uri.parse("res://"+ getApplication().getPackageName()+ R.mipmap.ic_launcher);
+        //Uri uri = Uri.parse("res://"+ getApplication().getPackageName()+ R.mipmap.ic_launcher);
+        Uri uri_net = Uri.parse("http://pic.58pic.com/58pic/11/13/74/96g58PICKft.jpg");
+
         Bitmap bmp = null;
         try {
             InputStream is = getAssets().open("j15.png");
@@ -40,10 +43,19 @@ public class FrescoAssetsActivity extends Activity{
         ImageView iv_res = (ImageView) findViewById(R.id.iv_res);
         ImageView iv_assets = (ImageView) findViewById(R.id.iv_assets);
         SimpleDraweeView sdv = (SimpleDraweeView) findViewById(R.id.sdv_simple);
+        SimpleDraweeView sdv_circle = (SimpleDraweeView) findViewById(R.id.sdv_circle);
         iv_res.setImageResource(R.mipmap.ic_launcher);
         iv_assets.setImageBitmap(bmp);
         //TODO Uri
-        //sdv.setImageURI(uri);
+        sdv.setImageURI(uri_net);
+
+        //TODO 设置圆角图片
+        RoundingParams roundingParams = RoundingParams.fromCornersRadius(5f);
+        roundingParams.setBorder(R.color.colorPrimary, 1.0f);
+        roundingParams.setRoundAsCircle(true);
+        sdv_circle.setImageURI(uri_net);
+        sdv_circle.getHierarchy().setRoundingParams(roundingParams);
+
 
     }
 }
